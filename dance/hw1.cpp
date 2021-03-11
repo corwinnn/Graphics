7cfe6bd81679d9b9af833649ff410dc1bac265a9
Mike <geller.m@phystech.edu>
2021-03-12 02:28:51 +0300

    codestyle fixes

--- dance/hw1.cpp
+++ dance/hw1.cpp
@@ -7,25 +7,27 @@
 
 // Include GLFW
 #include <GLFW/glfw3.h>
-GLFWwindow* window;
+
+GLFWwindow *window;
 
 // Include GLM
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+
 using namespace glm;
 
 #include <common/shader.hpp>
 
 
-double X(double t, double a=1, double b=1) {
+double X(double t, double a = 1, double b = 1) {
     return (a + b) * cos(t) - b * cos(t * ((a + b) / b));
 }
 
-double Y(double t, double a=1, double b=1) {
+double Y(double t, double a = 1, double b = 1) {
     return (a + b) * sin(t) - b * sin(t * ((a + b) / b));
 }
 
-void draw(GLuint& programID, GLuint& MatrixID, glm::mat4& MVP, GLuint& vertexbuffer, GLuint& colorbuffer, int cnt) {
+void draw(GLuint &programID, GLuint &MatrixID, glm::mat4 &MVP, GLuint &vertexbuffer, GLuint &colorbuffer, int cnt) {
     glUseProgram(programID);
 
     // Send our transformation to the currently bound shader,
@@ -41,7 +43,7 @@ void draw(GLuint& programID, GLuint& MatrixID, glm::mat4& MVP, GLuint& vertexbuf
             GL_FLOAT,           // type
             GL_FALSE,           // normalized?
             0,                  // stride
-            (void*)0            // array buffer offset
+            (void *) 0            // array buffer offset
     );
 
     // 2nd attribute buffer : colors
@@ -53,7 +55,7 @@ void draw(GLuint& programID, GLuint& MatrixID, glm::mat4& MVP, GLuint& vertexbuf
             GL_FLOAT,                         // type
             GL_FALSE,                         // normalized?
             0,                                // stride
-            (void*)0                          // array buffer offset
+            (void *) 0                          // array buffer offset
     );
 
     // Draw the triangle !
@@ -63,89 +65,88 @@ void draw(GLuint& programID, GLuint& MatrixID, glm::mat4& MVP, GLuint& vertexbuf
     glDisableVertexAttribArray(1);
 }
 
-int main( void )
-{
-	// Initialise GLFW
-	if( !glfwInit() )
-	{
-		fprintf( stderr, "Failed to initialize GLFW\n" );
-		getchar();
-		return -1;
-	}
+int main(void) {
+    // Initialise GLFW
+    if (!glfwInit()) {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        getchar();
+        return -1;
+    }
 
-	glfwWindowHint(GLFW_SAMPLES, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // To make MacOS happy; should not be needed
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// Open a window and create its OpenGL context
-	window = glfwCreateWindow( 1200, 1200, "Dance of Triangles", NULL, NULL);
-	if( window == NULL ){
-		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
-		getchar();
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
+    // Open a window and create its OpenGL context
+    window = glfwCreateWindow(1200, 1200, "Dance of Triangles", NULL, NULL);
+    if (window == NULL) {
+        fprintf(stderr,
+                "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n");
+        getchar();
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
 
-	// Initialize GLEW
-	glewExperimental = true; // Needed for core profile
-	if (glewInit() != GLEW_OK) {
-		fprintf(stderr, "Failed to initialize GLEW\n");
-		getchar();
-		glfwTerminate();
-		return -1;
-	}
+    // Initialize GLEW
+    glewExperimental = true; // Needed for core profile
+    if (glewInit() != GLEW_OK) {
+        fprintf(stderr, "Failed to initialize GLEW\n");
+        getchar();
+        glfwTerminate();
+        return -1;
+    }
 
-	// Ensure we can capture the escape key being pressed below
-	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    // Ensure we can capture the escape key being pressed below
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
-	// Dark blue background
-	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+    // Dark blue background
+    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
 
-	// Enable depth test
+    // Enable depth test
 //	glEnable(GL_DEPTH_TEST);
 //	// Accept fragment if it closer to the camera than the former one
 //	glDepthFunc(GL_LESS);
 
-	GLuint VertexArrayID;
-	glGenVertexArrays(1, &VertexArrayID);
-	glBindVertexArray(VertexArrayID);
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
 
-	// Create and compile our GLSL program from the shaders
-	GLuint programID = LoadShaders( "TransformVertexShader.vertexshader", "ColorFragmentShader.fragmentshader" );
-    GLuint programID2 = LoadShaders( "TransformVertexShader2.vertexshader", "ColorFragmentShader2.fragmentshader" );
-    GLuint programID3 = LoadShaders( "TransformVertexShader3.vertexshader", "ColorFragmentShader3.fragmentshader" );
+    // Create and compile our GLSL program from the shaders
+    GLuint programID = LoadShaders("TransformVertexShader.vertexshader", "ColorFragmentShader.fragmentshader");
+    GLuint programID2 = LoadShaders("TransformVertexShader2.vertexshader", "ColorFragmentShader2.fragmentshader");
+    GLuint programID3 = LoadShaders("TransformVertexShader3.vertexshader", "ColorFragmentShader3.fragmentshader");
 
-	// Get a handle for our "MVP" uniform
-	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+    // Get a handle for our "MVP" uniform
+    GLuint MatrixID = glGetUniformLocation(programID, "MVP");
     GLuint MatrixID2 = glGetUniformLocation(programID2, "MVP");
     GLuint MatrixID3 = glGetUniformLocation(programID3, "MVP");
 
 
     // Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
-	// Camera matrix
-	glm::mat4 View       = glm::lookAt(
-								glm::vec3(4,3,-3), // Camera is at (4,3,-3), in World Space
-								glm::vec3(0,0,0), // and looks at the origin
-								glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
-						   );
-	// Model matrix : an identity matrix (model will be at the origin)
-	glm::mat4 Model      = glm::mat4(1.0f);
-	// Our ModelViewProjection : multiplication of our 3 matrices
-	glm::mat4 MVP        = Projection * View * Model; // Remember, matrix multiplication is the other way around
+    glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+    // Camera matrix
+    glm::mat4 View = glm::lookAt(
+            glm::vec3(4, 3, -3), // Camera is at (4,3,-3), in World Space
+            glm::vec3(0, 0, 0), // and looks at the origin
+            glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
+    );
+    // Model matrix : an identity matrix (model will be at the origin)
+    glm::mat4 Model = glm::mat4(1.0f);
+    // Our ModelViewProjection : multiplication of our 3 matrices
+    glm::mat4 MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
 
-	// Our vertices. Tree consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
-	// A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
-	static const GLfloat g_vertex_buffer_data[] = {
-		-0.7f,-0.7f,-0.4f,
-		-0.7f, 0.7f, -0.4f,
-		 0.7f, 0.0f, -0.4f,
-	};
+    // Our vertices. Tree consecutive floats give a 3D vertex; Three consecutive vertices give a triangle.
+    // A cube has 6 faces with 2 triangles each, so this makes 6*2=12 triangles, and 12*3 vertices
+    static const GLfloat g_vertex_buffer_data[] = {
+            -0.7f, -0.7f, -0.4f,
+            -0.7f, 0.7f, -0.4f,
+            0.7f, 0.0f, -0.4f,
+    };
     static const GLfloat g_vertex_buffer_data2[] = {
-            0.7,-0.7f, 0.4f,
+            0.7, -0.7f, 0.4f,
             0.7f, 0.7f, 0.4f,
             -0.7f, 0.0f, 0.4f,
     };
@@ -154,33 +155,33 @@ int main( void )
     static const GLfloat g_vertex_buffer_data3[] = {
             q, 0, 0,
             0, 0, -q,
-            0, 2*q, 0,
+            0, 2 * q, 0,
 
             0, 0, -q,
-            0, 2*q, 0,
+            0, 2 * q, 0,
             -q, 0, 0,
 
-            0, 2*q, 0,
+            0, 2 * q, 0,
             -q, 0, 0,
             0, 0, q,
 
-            0, 2*q, 0,
+            0, 2 * q, 0,
             0, 0, q,
             q, 0, 0,
 
             q, 0, 0,
             0, 0, -q,
-            0, -2*q, 0,
+            0, -2 * q, 0,
 
             0, 0, -q,
-            0, -2*q, 0,
+            0, -2 * q, 0,
             -q, 0, 0,
 
-            0, -2*q, 0,
+            0, -2 * q, 0,
             -q, 0, 0,
             0, 0, q,
 
-            0, -2*q, 0,
+            0, -2 * q, 0,
             0, 0, q,
             q, 0, 0,
 
@@ -188,14 +189,14 @@ int main( void )
     };
 
     static const GLfloat g_color_buffer_data[] = {
-            0.8f,  0.1f,  0.14f,
-            0.09f,  0.8f,  0.16f,
-            0.07f,  0.03f,  0.844f,
+            0.8f, 0.1f, 0.14f,
+            0.09f, 0.8f, 0.16f,
+            0.07f, 0.03f, 0.844f,
     };
     static const GLfloat g_color_buffer_data2[] = {
-            0.0822f,  0.069f,  0.801f,
-            0.035f,  0.8202f,  0.0223f,
-            0.810f,  0.0747f,  0.0185f,
+            0.0822f, 0.069f, 0.801f,
+            0.035f, 0.8202f, 0.0223f,
+            0.810f, 0.0747f, 0.0185f,
     };
 
     static const GLfloat g_color_buffer_data3[] = {
@@ -237,15 +238,15 @@ int main( void )
     glEnable(GL_BLEND);
     glBlendFunc(GL_ONE, GL_ONE);
 
-	GLuint vertexbuffer;
-	glGenBuffers(1, &vertexbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+    GLuint vertexbuffer;
+    glGenBuffers(1, &vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
-	GLuint colorbuffer;
-	glGenBuffers(1, &colorbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+    GLuint colorbuffer;
+    glGenBuffers(1, &colorbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
 
     GLuint vertexbuffer2;
     glGenBuffers(1, &vertexbuffer2);
@@ -268,47 +269,48 @@ int main( void )
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data3), g_color_buffer_data3, GL_STATIC_DRAW);
     double x = 0, h = -3;
 
-	do{
+    do {
         // Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
         glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
         // Camera matrix
 
-        glm::mat4 View       = glm::lookAt(
-                glm::vec3(4*sin(h), X(x),Y(x)),// Camera is at (4,3,-3), in World Space
-                glm::vec3(0,0,0), // and looks at the origin
-                glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+        glm::mat4 View = glm::lookAt(
+                glm::vec3(4 * sin(h), X(x), Y(x)),// Camera is at (4,3,-3), in World Space
+                glm::vec3(0, 0, 0), // and looks at the origin
+                glm::vec3(0, 1, 0)  // Head is up (set to 0,-1,0 to look upside-down)
         );
-        x+=0.001;h+=0.001;
+        x += 0.001;
+        h += 0.001;
         // Model matrix : an identity matrix (model will be at the origin)
-        glm::mat4 Model      = glm::mat4(1.0f);
+        glm::mat4 Model = glm::mat4(1.0f);
         // Our ModelViewProjection : multiplication of our 3 matrices
-        glm::mat4 MVP        = Projection * View * Model; // Remember, matrix multiplication is the other way around
+        glm::mat4 MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
 
-		// Clear the screen
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        // Clear the screen
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         draw(programID, MatrixID, MVP, vertexbuffer, colorbuffer, 3);
         draw(programID2, MatrixID2, MVP, vertexbuffer2, colorbuffer2, 3);
-        draw(programID3, MatrixID3, MVP, vertexbuffer3, colorbuffer3, 3*8);
+        draw(programID3, MatrixID3, MVP, vertexbuffer3, colorbuffer3, 3 * 8);
 
 
         // Swap buffers
-		glfwSwapBuffers(window);
-		glfwPollEvents();
+        glfwSwapBuffers(window);
+        glfwPollEvents();
 
-	} // Check if the ESC key was pressed or the window was closed
-	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-		   glfwWindowShouldClose(window) == 0 );
+    } // Check if the ESC key was pressed or the window was closed
+    while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+           glfwWindowShouldClose(window) == 0);
 
-	// Cleanup VBO and shader
-	glDeleteBuffers(1, &vertexbuffer);
-	glDeleteBuffers(1, &colorbuffer);
-	glDeleteProgram(programID);
-	glDeleteVertexArrays(1, &VertexArrayID);
+    // Cleanup VBO and shader
+    glDeleteBuffers(1, &vertexbuffer);
+    glDeleteBuffers(1, &colorbuffer);
+    glDeleteProgram(programID);
+    glDeleteVertexArrays(1, &VertexArrayID);
 
-	// Close OpenGL window and terminate GLFW
-	glfwTerminate();
+    // Close OpenGL window and terminate GLFW
+    glfwTerminate();
 
-	return 0;
+    return 0;
 }
 
